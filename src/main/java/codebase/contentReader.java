9b4916d1777933700f491ab7e4d519610b2b942b
michel van de weetering <michel.van.de.weetering@caseware.nl>
2017-08-21 20:32:09 +0200

    mog default insulter

--- src/main/java/codebase/contentReader.java
+++ src/main/java/codebase/contentReader.java
@@ -1,11 +1,14 @@
 package codebase;
 
+import java.util.function.IntSupplier;
+
 public class contentReader {
 
+	private static boolean mogon;
+	
 	public static String GetContent(String Bericht, String Gebruiker) {
 
 		String Reactie = null;
-
 		Bericht = Bericht.toLowerCase();
 
 		switch (Bericht) {
@@ -17,14 +20,11 @@ public class contentReader {
 
 			if (Gebruiker.equalsIgnoreCase("Teefy")) {
 				Reactie = "Hello, beautiful /wink";
-			} 
-			else if (Gebruiker.equalsIgnoreCase("Icestorm")) {
+			} else if (Gebruiker.equalsIgnoreCase("Icestorm")) {
 				Reactie = "Hello, you Manly Man";
-			}
-			else if (Gebruiker.equalsIgnoreCase("Mog_no_1")) {
+			} else if (Gebruiker.equalsIgnoreCase("Mog_no_1")) {
 				Reactie = "All bow before the Great Leader and Master ! hi Mog :)";
-			}
-			else {
+			} else {
 				Reactie = "Hello, " + Gebruiker + ". Its good to see the plebs has found their way here";
 			}
 			break;
@@ -35,6 +35,9 @@ public class contentReader {
 		case "!hug":
 			Reactie = "c'mere, lets give you a hug !";
 			break;
+		case "!online":
+			Reactie = "Mogbot online. hello my minions!";
+			break;
 		case "!bringcoffee":
 			Reactie = "Get your own coffee. lazy wanker!";
 			break;
@@ -44,7 +47,7 @@ public class contentReader {
 		case "!joke":
 			Reactie = jokes.loadJoke();
 			break;
-		case "!name":	
+		case "!name":
 			Reactie = NameGenerator.generateName();
 			break;
 		case "!roo":
@@ -53,13 +56,61 @@ public class contentReader {
 		case "!shitgold":
 			Reactie = "The only thing you're shitting is :poop: ! ";
 			break;
-			
-		}
+		case "!spider":
+			Reactie = "mogbot watches as Mog gets eaten by a giant spider. ";
+			break;
+		case "!coffee":
+			Reactie = "Get your own coffee. do i look like the waiter ?";
+			break;
+		case "!wafflecunt":
+			Reactie = "All hail our Master Wafflecunt, VuduePriest. HAIL !";
+			break;
+		case "!rooballs":
+			Reactie = "Mog eats roo balls for breakfast!";
+			break;
+		case "!ibis":
+			Reactie = "http://i.imgur.com/uluX2Gd.gifv";
+			break;
+		case "!boo":
+			Reactie = "http://gph.is/17h4Snw";
+			break;
+		case "!aww":
+			Reactie = "http://gph.is/15zmlbN";
+			break;
+
+		case "!mogstatus":
+			Reactie = Boolean.toString(mogon);
+			break;
+	
+		default:
+			if (Gebruiker.equalsIgnoreCase("Mog_no_1") && mogon) {
+			Reactie = "Shut up " + loadInsults.returnInsult() + " !!";
+			break;
+			}
+				
+			}
+
+
 
 		if (Bericht.contains("!kick ")) {
 			String Victim = null;
 			Victim = Bericht.substring(6, Bericht.length());
 			Reactie = "/Kicks " + Victim + " in the balls";
+		}
+
+		if (Bericht.contains("!mogon") && !Gebruiker.equalsIgnoreCase("Mog_no_1")) {
+			mogon = true;
+		}
+		
+		if (Bericht.contains("!mogoff") && !Gebruiker.equalsIgnoreCase("Mog_no_1")) {
+			mogon = false;
+			
+		}
+	
+		if (Bericht.contains("!coffee ")) {
+			String Victim = null;
+			Victim = Bericht.substring(8, Bericht.length());
+			Reactie = " Mogbot serves a lovely steaming hot coffee to " + Victim;
 		}
 
 		if (Bericht.contains("!hug ")) {
@@ -72,7 +123,7 @@ public class contentReader {
 			String Victim = null;
 			Victim = Bericht.substring(6, Bericht.length());
 			Reactie = "/Slaps " + Victim + " accross the face. Ouch !!!";
-		}	
+		}
 
 		return Reactie;
 
@@ -80,7 +131,7 @@ public class contentReader {
 
 	public static void main(String[] args) {
 
-		System.out.println(GetContent("!slap teefy", "Teefy"));
+		System.out.println(GetContent("!coffee", "Teefy"));
 
 	}
 
